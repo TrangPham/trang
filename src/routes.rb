dfc462857af89921490f13b78610de823380bf57
TrangPham <trang.pham@cenx.com>
2013-11-19 15:40:45 -0500

    Added contact, cv pages

--- src/routes.rb
+++ src/routes.rb
@@ -7,5 +7,17 @@ module Trang
       haml :"index"
     end
 
+    get "/contact" do
+      haml :"contact"
+    end
+
+    get "/cv" do
+      haml :"cv"
+    end
+
+    get "/about" do
+      haml :"about"
+    end
+    
   end
 end
